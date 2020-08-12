001701ef2895e58eca413fd029077e088195456e
prasanth1367 <63450886+prasanth1367@users.noreply.github.com>
2020-08-12 19:15:53 +0530

    created positive no.c

---
+++ positive no.c
@@ -0,0 +1,13 @@
+ 
+list1 = [12, -7, 5, 64, -14]
+
+pos_nos = [num for num in list1 if num >= 0]
+ 
+print("Positive numbers in the list: ", *pos_nos)
+
+list2 = [12, 14, -95, 3]
+
+pos_nos = [num for num in list2 if num >= 0]
+ 
+print("Positive numbers in the list: ", *pos_nos)
+
